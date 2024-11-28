83bb869abb9eeaa9dfb23c8db49cec4981da6528
Сергей Владиславович Маклецов <smak.80@yandex.ru>
2024-11-28 12:43:44 +0300

    Добавьте файлы проекта.

---
+++ Complex2024/Complex.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Complex2024
+{
+    public class Complex
+    {
+        public double Re { get; set; }
+        public double Im { get; set; }
+        public double AbsoluteValue => Math.Sqrt(Re * Re + Im * Im);
+        public Complex Conjugate => new Complex(Re, -Im);
+
+        public Complex(double re, double im)
+        {
+            Re = re;
+            Im = im;
+        }
+
+        public Complex() { }
+        public override string ToString()
+        {
+            var sb = new StringBuilder();
+            if (Re != 0 || Im == 0)
+            {
+                sb.Append(Re);
+            }
+            if (Im != 0)
+            {
+                if (Im > 0 && Re != 0)
+                {
+                    sb.Append("+");
+                }
+                else
+                {
+                    sb.Append("-");
+                }
+                if (Math.Abs(Im) != 1)
+                {
+                    sb.Append(Math.Abs(Im));
+                }
+                sb.Append('i');
+            }
+            return sb.ToString();
+        }
+        public Complex Sum(Complex other) => new Complex(Re + other.Re, Im + other.Im);
+        public Complex Diff(Complex other) => new Complex(Re - other.Re, Im - other.Im);
+
+        public Complex Product(Complex other) => new Complex(Re * other.Re - Im * other.Im, Re * other.Im + Im * other.Re);
+        public Complex Quotient(Complex other)
+        {
+            double denominator = other.Re * other.Re + other.Im * other.Im;
+            var re = (Re * other.Re + Im * other.Im) / denominator;
+            var im = (Im * other.Re - Re * other.Im) / denominator;
+
+            return new(re, im);
+        }
+
+        public void SumAssign(Complex other)
+        {
+            Re += other.Re;
+            Im += other.Im;
+        }
+        public void DiffAssign(Complex other)
+        {
+            Re -= other.Re;
+            Im -= other.Im;
+        }
+        public void ProductAssign(Complex other)
+        {
+            var l = Re * other.Re - other.Im * Im;
+            Im = Im * other.Re + other.Im * Re;
+            Re = l;
+
+            /*var l = Re;
+            Re = Re * other.Re - other.Im * Im;
+            Im = Im * other.Re + other.Im * l;*/
+        }
+    }
+
+}
---
+++ Complex2024/Program.cs
@@ -0,0 +1,27 @@
+﻿using Complex2024;
+var z1 = new Complex();
+var z3 = new Complex(7, -2);
+var z2 = new Complex()
+{
+    Re = 3,
+    Im = 4
+};
+
+Console.WriteLine(z1);
+Console.WriteLine(z1.AbsoluteValue);
+Console.WriteLine(z1.Conjugate);
+Console.WriteLine(z2);
+Console.WriteLine(z2.AbsoluteValue);
+Console.WriteLine(z2.Conjugate);
+Console.WriteLine(z3);
+Console.WriteLine(z3.AbsoluteValue);
+Console.WriteLine(z3.Conjugate);
+Console.WriteLine(z3.Sum(z3.Conjugate));
+Console.WriteLine(z3.Diff(z3.Conjugate));
+
+var z4 = new Complex(1, -2);
+Complex z5 = new (3, 1);
+Console.WriteLine(z4.Quotient(z5));
+Console.WriteLine(z4.Product(z5));
+z4.ProductAssign(z5);
+Console.WriteLine(z4);
